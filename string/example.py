e248e70f4556fe99705ca1e011fb3c1148ef52ea
Yuzuru Nishimiya <sonyalamsyah.learn@gmail.com>
2022-02-17 23:36:29 +0700

    add content to example

---
+++ string/example.py
@@ -0,0 +1,9 @@
+# capitalize
+
+original_string = "hello world"
+
+capitalized_string = original_string.capitalize()
+center_string = original_string.center(23, "*")
+
+print(".capitalize(): ", capitalized_string)
+print(".ccenter(23, '*'): ", center_string)
